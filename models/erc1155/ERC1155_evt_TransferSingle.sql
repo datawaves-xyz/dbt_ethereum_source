bebeea4c59ed6f0746f1c7d27a805c4a4de17a2c
Jan-zou <zoujlin@gmail.com>
2022-06-07 17:58:10 +0800

    modify erc1155_transfersingle value type

--- models/erc1155/ERC1155_evt_TransferSingle.sql
+++ models/erc1155/ERC1155_evt_TransferSingle.sql
@@ -1,3 +1,13 @@
 select
-  *
+  evt_block_number,
+  evt_block_time,
+  evt_index,
+  evt_tx_hash,
+  contract_address,
+  operator,
+  from,
+  to,
+  id,
+  cast(value as double) as value,
+  dt
 from {{ ref('common_ERC1155_evt_TransferSingle') }}
